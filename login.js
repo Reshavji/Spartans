9209dbba54ec5b00b93c646d110df42cef25c3d9
Reshav ji <116823969+Reshavji@users.noreply.github.com>
2023-02-10 17:19:18 +0530

    Add files via upload

--- login.js
+++ login.js
@@ -9,3 +9,13 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+function openForm() {
+	document.getElementById("myForm").style.display = "block";
+	document.getElementById("first").style.display = "none";
+  }
+  
+  function closeForm() {
+	document.getElementById("myForm").style.display = "none";
+	document.getElementById("first").style.display = "block";
+  }
+  
